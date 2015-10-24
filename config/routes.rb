161e40305773e45176671d8c460c91b3eafb72d2
Shantall Medina <shantall@MacBook-Pro-2.local>
2015-10-24 16:01:04 -0700

    adding comments and the rest

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :comments
+  get 'pages/info'
+
+  get 'pages/info'
+
   resources :adventures
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
